c92ef0551d74d09c1ce4a8103bff0e0646196249
NadeeshanFernando <nadeeshanfernandz@gmail.com>
2026-02-25 23:57:41 +0530

    very basic for practice

--- src/main/java/com/practice/practice/exception/ApiError.java
+++
@@ -1,8 +0,0 @@
-package com.practice.practice.exception;
-
-public record ApiError(
-        String timestamp,
-        int status,
-        String error,
-        String message
-) {}
--- src/main/java/com/practice/practice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.practice.practice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
